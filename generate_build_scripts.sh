10d66d7e7ccca9d89d1e053ef5bb9aa7c9d42689
wangy <wangy-dd@glodon.com>
2023-08-10 15:34:06 +0800

    update build script

--- generate_build_scripts.sh
+++ generate_build_scripts.sh
@@ -1,16 +1,27 @@
 # step1. clone ai_algo_framework
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+BUILD_DIR=${SHELL_FOLDER}/build
+if [[ -d "${BUILD_DIR}" ]]; then
+  rm -fr ${BUILD_DIR}
+fi
 GIE_DIR=${SHELL_FOLDER}/build/gie
-mkdir -p ${GIE_DIR}
-git clone --recursive --single-branch --depth=1 -b feature/v2-dev-debug ssh://git@geekssh.glodon.com:7999/data-intelligence-service/ai_algo_framework.git ${GIE_DIR}
+if [[ ! -d "${GIE_DIR}" ]]; then
+  mkdir -p ${GIE_DIR}
+fi
+GIE_BRANCH_TAG=feature/v2-dev-debug
+git clone --recursive --single-branch --depth=1 -b ${GIE_BRANCH_TAG} ssh://git@geekssh.glodon.com:7999/data-intelligence-service/ai_algo_framework.git ${GIE_DIR}
 
 # step2. generate build script
-cd ${SHELL_FOLDER} 
-python  ./build.py -v --dryrun --backend=gie --backend=tensorrt --backend=python --backend=ensemble --build-type=Release --build-parallel=16 \
+python  ${SHELL_FOLDER}/build.py -v --dryrun --backend=gie --backend=ensemble --build-type=Release --build-parallel=16 \
         --endpoint=grpc --endpoint=http --enable-logging --enable-stats \
         --enable-metrics --enable-gpu-metrics --enable-tracing --enable-gpu \
         --enable-triton-gie-backend --enable-onnxruntime 
 
 # step3. run docker_build
 DOCKER_BUILD_FILE=${SHELL_FOLDER}/build/docker_build
-bash ${DOCKER_BUILD_FILE}
+if [[ -e "${DOCKER_BUILD_FILE}" ]]; then
+  bash ${DOCKER_BUILD_FILE}
+else
+  echo "file: ${DOCKER_BUILD_FILE} not exist"
+  exit 1
+fi 
